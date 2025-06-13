c1641ae9ea72d5478267dc8a76d4743164d2416f
Muksin Muksin <75759731+muki01@users.noreply.github.com>
2025-06-13 13:33:32 +0300

    Add OBD2_KLine.h File

---
+++ src/OBD2_KLine.h
@@ -0,0 +1,17 @@
+#ifndef OBD2_KLINE_H
+#define OBD2_KLINE_H
+
+#include <Arduino.h>
+
+class OBD2_KLine  {
+public:
+  OBD2_KLine(HardwareSerial& serialPort, long baudRate, uint8_t rxPin, uint8_t txPin);
+
+private:
+  HardwareSerial& _serial;
+  long _baudRate;
+  uint8_t _rxPin;
+  uint8_t _txPin;
+};
+
+#endif // OBD2_KLINE_H
