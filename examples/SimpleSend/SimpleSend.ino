cf73fb1cc6ac6abaca69d3f085660458836f8a9a
Muksin Muksin <75759731+muki01@users.noreply.github.com>
2025-06-17 01:23:04 +0300

    Change Example Code

--- examples/SimpleSend/SimpleSend.ino
+++ examples/SimpleSend/SimpleSend.ino
@@ -1,18 +1,32 @@
-#include <OBD2_KLine.h>
+#include "OBD2_KLine.h"
 
-OBD2_KLine kline(Serial1, 10400, 9, 10);
+OBD2_KLine kline(Serial1, 10400, 10, 11);
+
+bool connected = false;
 
 void setup() {
-  //kline.begin();
-  //kline.setProtocol("ISO14230_Fast");
-  //kline.setWriteDelay(5);
-  //kline.setDataRequestInterval(60);
-  while(!kline.init_OBD2()){
+  Serial.begin(115200);
+  Serial.println("OBD2 K-Line PID Reading Example");
 
-  }
+  kline.setProtocol("ISO14230_Fast");
+  kline.setWriteDelay(5);
+  kline.setDataRequestInterval(60);
+  kline.beginSerial();
+  Serial.println("OBD2 Starting.");
 }
 
 void loop() {
-  kline.writeData(read_LiveData, 0x0C);
-  delay(1000);
+  if (!connected) {
+    Serial.println("Trying to connect.");
+    connected = kline.init_OBD2();
+  } else {
+    int rpm = kline.getPID(0x0C);
+    Serial.print("Engine RPM: "), Serial.println(rpm);
+
+    int coolantTemp = kline.getPID(0x05);
+    Serial.print("Coolant Temp: "), Serial.print(coolantTemp), Serial.println(" C");
+
+    int speed = kline.getPID(0x0D);
+    Serial.print("Vehicle Speed: "), Serial.print(speed), Serial.println(" km/h");
+  }
 }
