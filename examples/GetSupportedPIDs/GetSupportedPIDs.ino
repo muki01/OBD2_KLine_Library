dddb85066328163451ea35e09d621cd4b6f407c5
Muksin Muksin <75759731+muki01@users.noreply.github.com>
2025-06-25 00:46:58 +0300

    Add GetSupportedPIDs example code

---
+++ examples/GetSupportedPIDs/GetSupportedPIDs.ino
@@ -0,0 +1,28 @@
+#include "OBD2_KLine.h"
+
+OBD2_KLine KLine(Serial1, 10400, 10, 11);
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println("OBD2 K-Line Clear DTC Example");
+  KLine.setDebug(Serial);
+
+  KLine.setProtocol("ISO14230_Fast");
+  KLine.setWriteDelay(5);
+  KLine.setDataRequestInterval(60);
+  KLine.beginSerial();
+  Serial.println("OBD2 Starting.");
+}
+
+void loop() {
+  if (KLine.initOBD2()) {
+    KLine.getSupportedLiveData();
+    delay(1000);
+    KLine.getSupportedFreezeFrame();
+    delay(1000);
+    KLine.getSupportedVehicleInfo();
+    delay(1000);
+    KLine.getSupportedData(0x01);
+    delay(1000);
+  }
+}
