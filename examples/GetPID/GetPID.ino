79aee35881b7c48bf06122a8b15164bde0cdff0e
Muksin Muksin <75759731+muki01@users.noreply.github.com>
2025-06-25 00:50:53 +0300

    Update GetPID example

--- examples/GetPID/GetPID.ino
+++ examples/GetPID/GetPID.ino
@@ -16,13 +16,13 @@ void setup() {
 
 void loop() {
   if (KLine.initOBD2()) {
-    int rpm = KLine.getPID(0x0C);
+    int rpm = KLine.getLiveData(0x0C);
     Serial.print("Engine RPM: "), Serial.println(rpm);
 
-    int coolantTemp = KLine.getPID(0x05);
+    int coolantTemp = KLine.getLiveData(0x05);
     Serial.print("Coolant Temp: "), Serial.print(coolantTemp), Serial.println(" C");
 
-    int speed = KLine.getPID(0x0D);
+    int speed = KLine.getLiveData(0x0D);  //KLine.getPID(0x01, 0x0D);
     Serial.print("Vehicle Speed: "), Serial.print(speed), Serial.println(" km/h");
     Serial.println();
   }
