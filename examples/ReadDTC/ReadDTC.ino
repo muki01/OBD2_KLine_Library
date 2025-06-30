f7975c022dd63cd8e267f8a82a23de0fdba8985d
Muksin Muksin <75759731+muki01@users.noreply.github.com>
2025-07-01 00:53:47 +0300

    Update ReadDTC.ino

--- examples/ReadDTC/ReadDTC.ino
+++ examples/ReadDTC/ReadDTC.ino
@@ -16,13 +16,28 @@ void setup() {
 
 void loop() {
   if (KLine.initOBD2()) {
-    int dtcLength = KLine.readDTCs();
-    if (dtcLength > 0) {
-      for (int i = 0; i < dtcLength; i++) {
-        String dtc = KLine.getDTC(i);
+    int storedDTCLength = KLine.readStoredDTCs();
+    //int storedDTCLength = KLine.readDTCs(0x03);
+    if (storedDTCLength > 0) {
+      for (int i = 0; i < storedDTCLength; i++) {
+        String dtc = KLine.getStoredDTC(i);
         Serial.println(dtc);
       }
       Serial.println();
+    } else {
+      Serial.println("No Stored DTCs");
+    }
+
+    int pendingDTCLength = KLine.readPendingDTCs();
+    //int pendingDTCLength = KLine.readDTCs(0x07);
+    if (pendingDTCLength > 0) {
+      for (int i = 0; i < pendingDTCLength; i++) {
+        String dtc = KLine.getPendingDTC(i);
+        Serial.println(dtc);
+      }
+      Serial.println();
+    } else {
+      Serial.println("No Pending DTCs");
     }
   }
 }
