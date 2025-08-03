be1aba58fb5580b994307873a948f8673999863e
Muksin Muksin <75759731+muki01@users.noreply.github.com>
2025-08-04 00:35:56 +0300

    Rename and update GetPID example to GetLiveData

--- examples/GetLiveData/GetLiveData.ino
+++ examples/GetLiveData/GetLiveData.ino
@@ -1,17 +1,17 @@
 #include "OBD2_KLine.h"  // Include the library for OBD2 K-Line communication
-// #include <AltSoftSerial.h>  // Optional alternative software serial (not used here)
-// AltSoftSerial Alt_Serial;   // Create an alternative serial object (commented out)
+//#include <AltSoftSerial.h>  // Optional alternative software serial (not used here)
+//AltSoftSerial Alt_Serial;   // Create an alternative serial object (commented out)
 
 // ---------------- Create an OBD2_KLine object for communication.
 OBD2_KLine KLine(Serial1, 10400, 10, 11);  // Uses Hardware Serial (Serial1) at 10400 baud, with RX on pin 10 and TX on pin 11.
-// OBD2_KLine KLine(Alt_Serial, 10400, 8, 9); // Uses AltSoftSerial at 10400 baud, with RX on pin 8 and TX on pin 9.
+//OBD2_KLine KLine(Alt_Serial, 10400, 8, 9); // Uses AltSoftSerial at 10400 baud, with RX on pin 8 and TX on pin 9.
 
 void setup() {
   Serial.begin(115200);  // Start the default serial (for logging/debugging)
-  Serial.println("OBD2 K-Line PID Reading Example");
+  Serial.println("OBD2 K-Line Get Live Data Example");
 
   KLine.setDebug(Serial);              // Optional: enable debug output on your chosen serial port
-  KLine.setProtocol("ISO14230_Fast");  // Set communication protocol to ISO14230 (also known as KWP2000 Fast Init). Default protocol: Automatic
+  KLine.setProtocol("ISO14230_Fast");  // Optional: Default protocol: Automatic. All protocols: ISO9141. ISO14230_Slow, ISO14230_Fast
   KLine.setWriteDelay(5);              // Optional: delay between bytes when writing to OBD (in milliseconds)
   KLine.setDataRequestInterval(60);    // Optional: delay between data reading (in milliseconds)
 
